536a506a0753f698dd47dd8355921466c042fbbd
Pablo Neves <pn.matheus@hotmail.com>
2022-11-10 14:13:22 -0300

    Add files via upload
    Menu bug fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
-window.onload = function(){
-  document.querySelector(".menu-opener").addEventListener("click", function(){
+window.onload = function clickMenu(){
+  document.querySelector(".menu-opener").addEventListener("click", function clickMenu(){
     if(document.querySelector(".menu nav ul").style.display == 'flex'){
         document.querySelector(".menu nav ul").style.display = 'none';
     } else {
       document.querySelector(".menu nav ul").style.display = 'flex';
     }
   })
-}
+} 
+
 
 function clickTop(){
   windows.scrollTo(0, 0)
